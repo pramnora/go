1f6b8b6491df02f8f3f1e8db5bc134e45a9a40e3
Paul Ramnora <paul@ramnora.com>
2015-03-18 18:36:04 +0000

    Create if03.go
    if/else if/else statement using Go programming language

---
+++ if03.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+   n:=3
+   if n==1{
+    fmt.Println("n=1")
+   } else if n==2 {
+    fmt.Println("n=2")
+   } else {
+    fmt.Println("n<>1/n<>2")
+   }
+}
