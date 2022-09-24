99a8dd0743de65337538cf562f955c7abe0d8d17
Paul Ramnora <pramnora@yahoo.com>
2022-09-24 18:49:18 +0100

    Create hw01E.go
    Added file: [hw01E.go]

---
+++ language/output/hw/hw01E.go
@@ -0,0 +1,12 @@
+package main
+import ("fmt")
+
+func main() {
+        some_text := greeting()
+	fmt.Println(some_text)
+}
+
+func greeting() string{
+ text := "Hello, world! (5)"
+ return (text)
+}
